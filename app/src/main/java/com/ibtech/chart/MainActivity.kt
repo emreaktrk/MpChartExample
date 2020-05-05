d84c736c1ef8d34a6d8fd2666ac9b4ab85e341ac
Emre Akturk <emreaktrk@hotmail.com>
2020-05-05 13:58:28 +0300

    Optimize imports

--- app/src/main/java/com/ibtech/chart/MainActivity.kt
+++ app/src/main/java/com/ibtech/chart/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.ibtech.chart
 
-import android.graphics.Color
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.TypefaceCompat
+import com.github.mikephil.charting.animation.ChartAnimator
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider
+import com.github.mikephil.charting.interfaces.datasets.ILineScatterCandleRadarDataSet
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.renderer.DataRenderer
+import com.github.mikephil.charting.renderer.LineChartRenderer
+import com.github.mikephil.charting.renderer.XAxisRenderer
+import com.github.mikephil.charting.utils.MPPointF
+import com.github.mikephil.charting.utils.Transformer
+import com.github.mikephil.charting.utils.ViewPortHandler
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -62,6 +76,7 @@ class MainActivity : AppCompatActivity() {
             background = ContextCompat.getDrawable(applicationContext, R.drawable.background_chart)
             description.isEnabled = false
             extraTopOffset = 24f
+            renderer = HighlightRender(this, animator, viewPortHandler)
             data = bind()
         }
     }
@@ -101,7 +116,49 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class MonthValueFormatter : ValueFormatter() {
+class MonthValueFormatter : IndexAxisValueFormatter() {
 
     override fun getFormattedValue(value: Float): String = "MAY"
+}
+
+class HighlightRender(
+    chart: LineDataProvider?,
+    animator: ChartAnimator?,
+    viewPortHandler: ViewPortHandler?
+) : LineChartRenderer(chart, animator, viewPortHandler) {
+
+    private var highlight: Highlight? = null
+
+    override fun drawExtras(c: Canvas) {
+        super.drawExtras(c)
+
+        highlight?.let {
+            with(Paint()) {
+                shader = LinearGradient(
+                    0f,
+                    0f,
+                    0f,
+                    c.height.toFloat(),
+                    intArrayOf(Color.WHITE, Color.TRANSPARENT),
+                    null,
+                    Shader.TileMode.CLAMP
+                )
+                c.drawRoundRect(
+                    it.drawX - 50f,
+                    25f,
+                    it.drawX + 50f,
+                    c.height.toFloat(),
+                    20f,
+                    20f,
+                    this
+                )
+            }
+        }
+    }
+
+    override fun drawHighlighted(c: Canvas?, indices: Array<out Highlight>?) {
+        super.drawHighlighted(c, indices)
+
+        highlight = indices?.firstOrNull()
+    }
 }
